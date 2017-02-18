2031fc6813ad936ccfb937207e44d2844b359e1e
Scott O'Malley <scottyomalley@gmail.com>
2017-02-18 15:33:16 +0000

    Initial Commit

---
+++ AncestryCodeTestFrontend/webpack.config.js
@@ -0,0 +1,25 @@
+const path = require('path');
+const webpack = require('webpack'); //to access built-in plugins
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+
+module.exports = {
+  entry: './app/app.js',
+  devtool: 'cheap-module-eval-source-map',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+    publicPath: 'dist'
+  },
+  module: {
+    rules: [
+      { test: /\.scss$/, use: ExtractTextPlugin.extract('css-loader?sourceMap!postcss-loader!sass-loader?sourceMap')},
+      { test: /\.(js|jsx)$/, use: 'babel-loader' },
+      { test: /\.html$/, use: 'raw-loader' },
+    ]
+  },
+  plugins: [
+    new webpack.optimize.UglifyJsPlugin(),
+    new ExtractTextPlugin('styles/main.css')
+  ]
+};
